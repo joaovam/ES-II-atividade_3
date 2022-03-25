8228c266d755a492646d751f0ea9d2cf0710cfc4
Joao Amorim <amorimvictorjoao3@gmail.com@users.noreply.github.com>
2022-03-25 16:58:10 -0300

    começando CriminosoCT

---
+++ src/main/java/com/jcg/hibernate/crud/operations/controle/CriminosoCT.java
@@ -0,0 +1,14 @@
+package com.jcg.hibernate.crud.operations.controle;
+
+
+import com.jcg.hibernate.crud.operations.Crime;
+import com.jcg.hibernate.crud.operations.DbOperations;
+
+
+public class CriminosoCT {
+    DbOperations operations = new DbOperations();
+    public void createCriminoso(Crime c) {
+
+    }
+
+}
